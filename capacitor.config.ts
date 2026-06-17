dc18aa1c6bd1186f6f8359224e1870cfc598cb9d
FieteP <cleeveny2003@gmail.com>
2026-06-18 01:30:34 +0200

    Added Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lifeos.app',
+  appName: 'life-os',
+  webDir: 'dist'
+};
+
+export default config;
