697df92fd5c81822ff25b69983b236073e709322
FieteP <cleeveny2003@gmail.com>
2026-06-18 21:11:24 +0200

    Remove leftover AppPickerPlugin import

--- android/app/src/main/java/com/lifeos/app/MainActivity.java
+++ android/app/src/main/java/com/lifeos/app/MainActivity.java
@@ -2,12 +2,10 @@ package com.lifeos.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.lifeos.apppicker.AppPickerPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        registerPlugin(AppPickerPlugin.class);
     }
 }
